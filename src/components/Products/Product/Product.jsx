4a16f8a457f205f3ce61821aec502212971efcfd
amos-kibet <kibetamos97@gmail.com>
2021-08-03 05:45:53 +0300

    Create NavBar component

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
 
+import useStyles from './styles';
+
 const Product = () => {
+    const classes = useStyles();
     return(
         <Card className={classes.root}>
             <CardMedia className={classes.media} image='' title={product.name} />
@@ -15,7 +18,7 @@ const Product = () => {
                         {product.price}
                     </Typography>
                 </div>
-                <Typography variant='h2' color='textSecondary'>{product.description}</Typography>
+                <Typography variant='body2' color='textSecondary'>{product.description}</Typography>
             </CardContent>
             <CardActions disableSpacing className={classes.cardActions}>
                 <IconButton aria-label='Add To Cart'>
